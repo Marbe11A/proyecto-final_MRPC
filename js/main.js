995e40e2e7a61aa38934ee789e1708eef151907f
Marbe11A <86388931+Marbe11A@users.noreply.github.com>
2021-07-07 21:07:23 -0500

    css
    Construcción

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// main.js Tipos de variables
+
+var texto = "hola como estas"; //texto 
+var x = 20; //numericas
+var flag= true; //boolean
+
+// #panfilo
+
+var starts=["hola crayola", "que pasop", "hey", "hello"]
+var chat=["woow", ["super", "que bien", "no"], "como crees"];
+
+function saludar(){
+	alert(starts[1]);
+}
+
+
+//alert ("Hola, si funciona el alert");
+
+//console.log(texto);
+
